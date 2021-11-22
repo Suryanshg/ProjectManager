271ab339f12704b248b6e1c5b6e5b74413ed2a11
o355 <owen@owenthe.ninja>
2021-11-22 14:14:01 -0500

    index page

--- frontend2/js/index/index.js
+++ frontend2/js/index/index.js
@@ -1,38 +1,43 @@
 class Index {
-    constructor(projform, errordiv, successdiv, urlspan) {
+    constructor(projform, errordiv, successdiv, urlspan, errorspan, urlhref) {
         this.projform = projform
         this.errordiv = errordiv
         this.successdiv = successdiv
         this.urlspan = urlspan
+        this.errorspan = errorspan
+        this.urlhref = urlhref
 
         this.apiurl = "https://2xt2u5o36m.execute-api.us-east-2.amazonaws.com/api/"
     }
 
     createProject() {
         var request = new XMLHttpRequest();
-        var data = {"projectName": $(this.projform).val()}
+        var data = {"projectName": this.projform.val()}
 
+        console.log(data)
         fetch(this.apiurl + "project/createProject", {
             method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
             body: JSON.stringify(data)
         })
         .then(response => response.json())
         .then(response => {
-            console.log(response)
             if (response['statusCode'] != 200) {
-                console.log(this.errordiv)
+                this.errorspan.html("")
                 this.errordiv.show()
                 this.successdiv.hide()
+                if (response['statusCode'] == 422) {
+                    this.errorspan.html("A project with the same name already exists.")
+                } else if (response['statusCode'] == 400) {
+                    this.errorspan.html("Lambda returned a critical error (400).")
+                }
                 return
             }
 
             $(this.errordiv).hide()
             $(this.successdiv).show()
-            $(this.urlspan).html(response['url'])
+            $(this.urlspan).html(response['url'].substring(1))
+            // Issue in the API where the URL is padded with 1 extra character.
+            $(this.urlhref).attr("href", response['url'].substring(1))
         })
 
     }
