85a7a127c1df9a4e8f3fa7575248194a86fc8006
ttpyorre <ttpyorre@wpi.edu>
2021-11-23 15:42:06 -0500

    First test case reuploaded.

---
+++ ProjectManager/src/test/java/projectmanager/handler/CreateProjectHandlerTest.java
@@ -0,0 +1,37 @@
+package projectmanager.handler;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+import projectmanager.http.CreateProjectRequest;
+import projectmanager.http.CreateProjectResponse;
+
+public class CreateProjectHandlerTest extends LambdaTest {
+
+	void testInput(String incoming, int outgoing) throws IOException {
+		CreateProjectHandler handler = new CreateProjectHandler();
+		CreateProjectRequest req = new Gson().fromJson(incoming, CreateProjectRequest.class);
+		CreateProjectResponse response = handler.handleRequest(req, createContext("create project"));
+
+		assertEquals(outgoing, response.statusCode);
+	}
+
+	@Test
+	public void createProjectTest(){  
+		String SAMPLE_INPUT_STRING = "{\"projectName\": \"test900\" }";
+		int RESULT = 200;
+
+		try {
+			testInput(SAMPLE_INPUT_STRING, RESULT);
+		} catch(IOException ioe) {
+			Assert.fail("invalid: " + ioe.getMessage());
+		}
+	}
+
+}
