8a0353dc12048191ec958d17a2ce634313877cdd
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2021-12-04 10:14:10 -0500

    Created team page (get/delete implemented)

---
+++ frontend2/js/team/team.js
@@ -0,0 +1,55 @@
+class Team {
+  constructor(header, topdiv) {
+    this.header = header;
+    this.topdiv = topdiv;
+
+    this.apiurl = getApiUrl();
+
+    this.render();
+  }
+  deleteTask(teammateid, elementid) {
+    fetch(this.apiurl + "project/teammates/deleteTeammate", {
+      method: "POST",
+      body: JSON.stringify({ teammateid }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response["statusCode"] != 200) {
+          $(`#${elementid}`).show();
+          return;
+        }
+        window.location.reload();
+      });
+  }
+  render() {
+    fetch(this.apiurl + "project?project=" + getParameterByName("project"), {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        $(this.teamHeader).empty();
+        $(this.topdiv).empty();
+        if (response["statusCode"] != 200) {
+          if (response["statusCode"] == 422) {
+            $(this.header).html("A project with this ID doesn't exist.");
+          } else {
+            $(this.header).html("Couldn't load project due to an error.");
+          }
+
+          return;
+        }
+        $(this.header).html("Teammate List");
+        const project = response["project"];
+        const projectid = project.id;
+        const teammates = project.teammates;
+        $(this.topdiv).append("");
+        for (const teammate of teammates) {
+          const { id, name, tasks } = teammate;
+          const teammateClass = new Teammate("team", id, name, tasks);
+          $(this.topdiv).append(teammateClass.render());
+        }
+
+        //$(this.header).html(response["project"]["name"]);
+      });
+  }
+}
---
+++ frontend2/js/team/teammate.js
@@ -0,0 +1,40 @@
+class Teammate {
+  constructor(taskVar, id, name, tasks) {
+    this.taskVar = taskVar;
+    this.id = id;
+    this.name = name;
+    this.tasks = tasks;
+    this.apiurl = getApiUrl();
+  }
+
+  render() {
+    var archivedtext = "";
+    if (this.archived) {
+      archivedtext = "disabled";
+    }
+    let html = `
+    <div class="row mb-3">
+      <hr>
+      <div class="col s12">
+        <h4>${this.name}</h4>
+        Tasks: ${this.tasks.map((task) => task.title)} <br/> <br/>
+        <button type="button" class="btn btn-danger" onclick="${
+          this.taskVar
+        }.deleteTask('${this.id}', 'taskDeletionError${
+      this.id
+    }')">Delete</button>
+        <div class="row" id="taskDeletionError${this.id}" style="display: none">
+        <div class="col s12">
+          <hr />
+          <p>
+            There was an error deleting your teammate.
+            <span id="projectCreationErrorDetailed"></span>
+          </p>
+        </div>
+      </div>
+      </div>
+    </div>
+        `;
+    return html;
+  }
+}
