2e176b70893328eb2297eb8bfb99876af9b2ef04
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2021-12-15 16:34:25 -0500

    removing archive middleware from delete project handler

--- ProjectManager/src/main/java/projectmanager/handler/DeleteProjectHandler.java
+++ ProjectManager/src/main/java/projectmanager/handler/DeleteProjectHandler.java
@@ -7,14 +7,10 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import projectmanager.db.ProjectDAO;
 import projectmanager.http.DeleteProjectRequest;
 import projectmanager.http.DeleteProjectResponse;
-import projectmanager.http.GenericResponse;
-import projectmanager.middleware.ProjectArchived;
 
 public class DeleteProjectHandler implements RequestHandler<DeleteProjectRequest, DeleteProjectResponse> {
 
 	public LambdaLogger logger = null;
-
-	ProjectArchived archivedMiddleware = new ProjectArchived();
 
 	@Override
 	public DeleteProjectResponse handleRequest(DeleteProjectRequest req, Context context) {
@@ -28,15 +24,11 @@ public class DeleteProjectHandler implements RequestHandler<DeleteProjectRequest
 		ProjectDAO dao = new ProjectDAO();
 
 		try {
-			GenericResponse archived = archivedMiddleware.getArchived(req.projectid, context);
-			if (archived.statusCode == 200) {
-				if (dao.deleteProject(req.projectid))
-					response = new DeleteProjectResponse(true, 200);
-				else
-					response = new DeleteProjectResponse(422,
-							"Unable to delete project, no project with the specified id exists");
-			} else
-				response = new DeleteProjectResponse(archived.statusCode, archived.error);
+			if (dao.deleteProject(req.projectid))
+				response = new DeleteProjectResponse(true, 200);
+			else
+				response = new DeleteProjectResponse(422,
+						"Unable to delete project, no project with the specified id exists");
 		} catch (Exception e) {
 			response = new DeleteProjectResponse(400,
 					"Unable to delete project:" + req.projectid + "(" + e.getMessage() + ")");
