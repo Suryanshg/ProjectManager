50b3e050381c84c728bcabfc41f94597d6eafdd8
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2021-12-15 09:54:12 -0500

    Fixing test to create a new project, teammate, and task

--- ProjectManager/src/test/java/projectmanager/handler/AssignUnassignTeammateHandlerTest.java
+++ ProjectManager/src/test/java/projectmanager/handler/AssignUnassignTeammateHandlerTest.java
@@ -6,6 +6,9 @@ import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
 
+import projectmanager.db.ProjectDAO;
+import projectmanager.db.TaskDAO;
+import projectmanager.db.TeammateDAO;
 import projectmanager.handler.AssignTeammateHandler;
 import projectmanager.http.AssignTeammateRequest;
 import projectmanager.http.AssignTeammateResponse;
@@ -13,6 +16,9 @@ import projectmanager.http.AssignTeammateResponse;
 import projectmanager.handler.UnassignTeammateHandler;
 import projectmanager.http.UnassignTeammateRequest;
 import projectmanager.http.UnassignTeammateResponse;
+import projectmanager.model.Project;
+import projectmanager.model.Task;
+import projectmanager.model.Teammate;
 
 public class AssignUnassignTeammateHandlerTest extends LambdaTest {
   void testAssignInput(String incoming, int outgoing) throws Exception {
@@ -31,12 +37,25 @@ public class AssignUnassignTeammateHandlerTest extends LambdaTest {
     assertEquals(outgoing, response.statusCode);
   }
 
-  // This is not a test (for now)
-  public void assignUnassignTeammateTestPasses() {
-    // TODO problem occurs because project doesn't exist
-    String projectid = "0bc22c80-a9d6-43a1-b1f2-7fba045eae0b";
-    String taskid = "09bc2552-142b-4486-8a56-9e9cde6134d1";
-    String teammateid = "9d118e43-e808-4f65-89f9-3927d6fa9960";
+  @Test
+  public void assignUnassignTeammateTestPasses() throws Exception {
+
+    ProjectDAO projectDAO = new ProjectDAO();
+    TeammateDAO teammateDAO = new TeammateDAO();
+    TaskDAO taskDAO = new TaskDAO();
+
+    projectDAO.addProject(new Project("new proj"));
+    Project project = projectDAO.getProject("new proj");
+    String projectid = project.id.toString();
+
+    teammateDAO.addTeammate(new Teammate("TestDelete Teammate"), projectid);
+    Teammate teammate = teammateDAO.getTeammateByNameAndProjectId("TestDelete Teammate", projectid);
+    String teammateid = teammate.id.toString();
+
+    taskDAO.addTask(new Task("TestDelete Teammate"), null, projectid);
+    Task task = taskDAO.getTaskByTitleAndProjectId("TestDelete Teammate", projectid);
+    String taskid = task.id.toString();
+
     String SAMPLE_INPUT_STRING = "{\"projectid\": \"" + projectid + "\", \"taskid\": \"" + taskid
         + "\", \"teammateid\": \"" + teammateid + "\" }";
     int RESULT = 200;
@@ -44,6 +63,7 @@ public class AssignUnassignTeammateHandlerTest extends LambdaTest {
     try {
       testAssignInput(SAMPLE_INPUT_STRING, RESULT);
       testUnassignInput(SAMPLE_INPUT_STRING, RESULT);
+      projectDAO.deleteProject(projectid);
     } catch (Exception ioe) {
       Assert.fail("invalid: " + ioe.getMessage());
     }
